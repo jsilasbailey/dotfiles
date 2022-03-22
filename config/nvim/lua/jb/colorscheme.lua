33470a3bf0e7ba57bb6e465eb444796224c929e5
Jesse Bailey <jbailey117@gmail.com>
2022-03-22 13:00:54 -0400

    Format colorscheme.lua

--- config/nvim/lua/jb/colorscheme.lua
+++ config/nvim/lua/jb/colorscheme.lua
@@ -2,6 +2,6 @@ local colorscheme = "aquarium"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  vim.notify("Colorscheme: " .. colorscheme .. " not found!")
-  return
+	vim.notify("Colorscheme: " .. colorscheme .. " not found!")
+	return
 end
