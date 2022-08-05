2a891229eb73f787a33966f2a3fe6792bbd18aaf
Jesse Bailey <jbailey117@gmail.com>
2022-08-05 10:49:24 -0400

    `sort` options

--- config/nvim/lua/jb/options.lua
+++ config/nvim/lua/jb/options.lua
@@ -1,9 +1,9 @@
 vim.o.backup = false
 vim.o.clipboard = "unnamedplus"
-vim.o.number = true
-vim.o.mouse = "a"
 vim.o.cursorline = true
+vim.o.mouse = "a"
+vim.o.number = true
+vim.o.shiftround = true
 vim.o.splitbelow = true
 vim.o.splitright = true
-vim.o.shiftround = true
 vim.o.termguicolors = true
