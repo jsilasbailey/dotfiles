f071eb468ead273edd3aeb826b83fb0f9501bfe1
Jesse Bailey <jbailey117@gmail.com>
2022-08-05 10:47:38 -0400

    Remove global shiftwidth
    Each language buffer handles this just fine.

--- config/nvim/lua/jb/options.lua
+++ config/nvim/lua/jb/options.lua
@@ -1,18 +1,9 @@
 vim.o.backup = false
-
 vim.o.clipboard = "unnamedplus"
-
 vim.o.number = true
-
 vim.o.mouse = "a"
 vim.o.cursorline = true
-
 vim.o.splitbelow = true
 vim.o.splitright = true
-
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
 vim.o.shiftround = true
-vim.o.expandtab = true
-
 vim.o.termguicolors = true
