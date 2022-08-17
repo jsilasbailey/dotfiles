4c869e9db3178888f3e1e262cb919d1cf3df29f7
Jesse Bailey <jesse@jsilasbailey.com>
2022-08-17 09:30:27 -0400

    (nvim) Configure native fzf for telescope

--- config/nvim/plugin/telescope.lua
+++ config/nvim/plugin/telescope.lua
@@ -30,6 +30,12 @@ telescope.setup({
 		--   extension_config_key = value,
 		-- }
 		-- please take a look at the readme of the extension you want to configure
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true,
+			override_file_sorter = true,
+			case_mode = "smart_case"
+		}
 	},
 })
 
