1e613d55b133b64a3c4423dc9ba436c71030c62e
Jesse Bailey <jesse@jsilasbailey.com>
2022-08-22 16:35:50 -0400

    (brew) Simplify completion setup
    - Guard brew completion setup behind the existence of the `brew`
      executable
    - Use `brew --prefix` instead of explicit arch specific prefix paths to
      add brew completion to FPATH

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -1,9 +1,9 @@
 # load our own completion functions
-fpath=(~/.zsh/completion /usr/local/share/zsh/site-functions /opt/homebrew/share/zsh/site-functions $fpath)
+fpath=(~/.zsh/completion $fpath)
 
 # https://formulae.brew.sh/formula/zsh-completions
 if type brew &>/dev/null; then
-  FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+  FPATH=$(brew --prefix)/share/zsh-completions:$(brew --prefix)/share/zsh/site-functions:$FPATH
 fi
 
 # completion; use cache if updated within 24h
