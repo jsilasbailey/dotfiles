57d21d4ccc508029522d774e76adf603f5950f64
Jesse Bailey <jesse@jsilasbailey.com>
2022-10-12 15:04:25 -0400

    (nvim) Set expandtab

--- config/nvim/lua/jb/options.lua
+++ config/nvim/lua/jb/options.lua
@@ -3,6 +3,7 @@ vim.o.clipboard = "unnamedplus"
 vim.o.cursorline = true
 vim.o.mouse = "a"
 vim.o.number = true
+vim.bo.expandtab = true
 vim.o.shiftround = true
 vim.o.splitbelow = true
 vim.o.splitright = true
