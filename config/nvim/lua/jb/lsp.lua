60b00fc8af8dc16df704c5a1ed30f39b9df9fced
Jesse Bailey <jesse@jsilasbailey.com>
2023-04-04 09:29:49 -0400

    (nvim) Check for lsp capabilities
    Before setting up commands to use LSP capabilities for a buffer, check
    that the given client supports those capabilities.
    
    Also:
    - Group creating vim keymaps with the command that they map to.
    - Move required module lines to top of file
    - Formatting changes

--- config/nvim/lua/jb/lsp.lua
+++ config/nvim/lua/jb/lsp.lua
@@ -1,62 +1,93 @@
-local lspconfig_status, lsp_config = pcall(require, "lspconfig")
-if not lspconfig_status then
-	return
-end
-
-local mason_status, mason = pcall(require, "mason")
-if not mason_status then
-	return
-end
-local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
-if not mason_lspconfig_status then
-	return
-end
-
-mason.setup({})
-mason_lspconfig.setup({
+require("mason").setup()
+require("mason-lspconfig").setup({
 	ensure_installed = {
 		"emmet_ls",
+		"ruby_ls",
 		"rust_analyzer",
 		"tailwindcss",
 		"tsserver",
 	},
 })
 
-local create_lsp_user_commands = function(bufnr)
+local lsp_config = require("lspconfig")
+local null_ls = require("null-ls")
+
+local client_capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities(client_capabilities)
+
+local create_lsp_user_commands = function(client, bufnr)
+	local opts = { buffer = bufnr }
+
 	vim.api.nvim_buf_create_user_command(bufnr, "LspDiagnosticLine", vim.diagnostic.open_float, {})
 	vim.api.nvim_buf_create_user_command(bufnr, "LspDiagnosticNext", vim.diagnostic.goto_next, {})
 	vim.api.nvim_buf_create_user_command(bufnr, "LspDiagnosticPrev", vim.diagnostic.goto_prev, {})
 	vim.api.nvim_buf_create_user_command(bufnr, "LspDiagnosticSetLoclist", vim.diagnostic.setloclist, {})
-
-	vim.api.nvim_buf_create_user_command(bufnr, "LspDefintion", vim.lsp.buf.definition, {})
-	vim.api.nvim_buf_create_user_command(bufnr, "LspCodeAction", vim.lsp.buf.code_action, {})
-	vim.api.nvim_buf_create_user_command(bufnr, "LspFormat", vim.lsp.buf.formatting, {})
-	vim.api.nvim_buf_create_user_command(bufnr, "LspHover", vim.lsp.buf.hover, {})
-	vim.api.nvim_buf_create_user_command(bufnr, "LspImplementation", vim.lsp.buf.implementation, {})
-	vim.api.nvim_buf_create_user_command(bufnr, "LspReferences", vim.lsp.buf.references, {})
-	vim.api.nvim_buf_create_user_command(bufnr, "LspRename", vim.lsp.buf.rename, {})
-	vim.api.nvim_buf_create_user_command(bufnr, "LspSignatureHelp", vim.lsp.buf.signature_help, {})
-	vim.api.nvim_buf_create_user_command(bufnr, "LspTypeDefinition", vim.lsp.buf.type_definition, {})
-end
-
-local on_lsp_attach = function(client, bufnr)
-	create_lsp_user_commands(bufnr)
-
-	local opts = { buffer = bufnr, noremap = true, silent = true }
-
-	vim.keymap.set("n", "K", ":LspHover<cr>", opts)
 	vim.keymap.set("n", "<leader>d", ":LspDiagnosticLine<cr>", opts)
 	vim.keymap.set("n", "[d", ":LspDiagnosticPrev<cr>", opts)
 	vim.keymap.set("n", "]d", ":LspDiagnosticNext<cr>", opts)
-	vim.keymap.set("n", "<leader>a", ":LspCodeAction<cr>", opts)
-	vim.keymap.set("n", "gd", ":LspDefintion<cr>", opts)
-	vim.keymap.set("n", "gy", ":LspTypeDefinition<cr>", opts)
-	vim.keymap.set("n", "gR", ":TroubleToggle lsp_references<cr>", opts)
-	vim.keymap.set("n", "<leader>f", ":LspFormat<cr>", opts)
+
+	if client.supports_method("textDocument/codeAction") then
+		vim.api.nvim_buf_create_user_command(bufnr, "LspCodeAction", vim.lsp.buf.code_action, { range = nil })
+		vim.keymap.set("n", "<leader>a", ":LspCodeAction<cr>", opts)
+	end
+
+	if client.supports_method("textDocument/definition") then
+		vim.api.nvim_buf_create_user_command(bufnr, "LspDefintion", vim.lsp.buf.definition, {})
+		vim.keymap.set("n", "gd", ":LspDefintion<cr>", opts)
+	end
+
+	if client.supports_method("textDocument/hover") then
+		vim.api.nvim_buf_create_user_command(bufnr, "LspHover", vim.lsp.buf.hover, {})
+		vim.keymap.set("n", "K", ":LspHover<cr>", opts)
+	end
+
+	if client.supports_method("textDocument/implementation") then
+		vim.api.nvim_buf_create_user_command(bufnr, "LspImplementation", vim.lsp.buf.implementation, {})
+	end
+
+	if client.supports_method("textDocument/typeDefinition") then
+		vim.api.nvim_buf_create_user_command(bufnr, "LspTypeDefinition", vim.lsp.buf.type_definition, {})
+		vim.keymap.set("n", "gy", ":LspTypeDefinition<cr>", opts)
+	end
+
+	if client.supports_method("textDocument/signatureHelp") then
+		vim.api.nvim_buf_create_user_command(bufnr, "LspSignatureHelp", vim.lsp.buf.signature_help, {})
+	end
+
+	local filtered_formatting = function()
+		vim.lsp.buf.format({
+			-- TODO: Depend on abstractions not concretions
+			filter = function(ls_client)
+				return ls_client.name ~= "tsserver"
+			end,
+			bufnr = bufnr,
+		})
+	end
+
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_buf_create_user_command(bufnr, "LspFormat", filtered_formatting, {})
+		vim.keymap.set("n", "<leader>f", ":LspFormat<cr>", opts)
+	end
+
+	if client.supports_method("textDocument/rangeFormatting") then
+		vim.keymap.set("v", "<leader>f", filtered_formatting, opts)
+	end
+
+	if client.supports_method("textDocument/references") then
+		vim.api.nvim_buf_create_user_command(bufnr, "LspReferences", vim.lsp.buf.references, {})
+	end
+
+	if client.supports_method("textDocument/rename") then
+		vim.api.nvim_buf_create_user_command(bufnr, "LspRename", vim.lsp.buf.rename, {})
+	end
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+local on_lsp_attach = function(client, bufnr)
+	create_lsp_user_commands(client, bufnr)
+
+	-- TODO: Move to trouble config
+	vim.keymap.set("n", "gR", ":TroubleToggle lsp_references<cr>", { buffer = bufnr })
+end
 
 require("neodev").setup({})
 lsp_config.lua_ls.setup({
@@ -64,13 +95,10 @@ lsp_config.lua_ls.setup({
 	capabilities = capabilities,
 })
 
-lsp_config.solargraph.setup({
-	capabilities = capabilities,
-	on_attach = on_lsp_attach,
-	init_options = {
-		formatting = false,
-	},
-})
+-- TODO: Ruby LSP Not working the way I would expect
+-- lsp_config.ruby_ls.setup({
+-- 	capabilities = capabilities,
+-- })
 
 require("typescript").setup({
 	server = {
@@ -82,15 +110,8 @@ require("typescript").setup({
 			vim.api.nvim_buf_create_user_command(bufnr, "LspRemoveUnused", "TypescriptRemoveUnused", {})
 			vim.api.nvim_buf_create_user_command(bufnr, "LspRenameFile", "TypescriptRenameFile", {})
 
-			-- Delegate to prettier
-			client.resolved_capabilities.document_formatting = false
-			client.resolved_capabilities.document_range_formatting = false
-
 			on_lsp_attach(client, bufnr)
 		end,
-		init_options = {
-			formatting = false,
-		},
 	},
 })
 
@@ -118,11 +139,6 @@ lsp_config.emmet_ls.setup({
 		"vue",
 	},
 })
-
-local status_ok, null_ls = pcall(require, "null-ls")
-if not status_ok then
-	return
-end
 
 local eslint_options = {
 	prefer_local = "node_modules/.bin",
@@ -180,15 +196,7 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 local on_null_ls_attach = function(client, bufnr)
-	create_lsp_user_commands(bufnr)
-
-	local opts = { buffer = bufnr, noremap = true, silent = true }
-
-	vim.keymap.set("n", "<leader>d", ":LspDiagnosticLine<cr>", opts)
-	vim.keymap.set("n", "[d", ":LspDiagnosticPrev<cr>", opts)
-	vim.keymap.set("n", "]d", ":LspDiagnosticNext<cr>", opts)
-	vim.keymap.set("n", "<leader>a", ":LspCodeAction<cr>", opts)
-	vim.keymap.set("n", "<leader>f", ":LspFormat<cr>", opts)
+	create_lsp_user_commands(client, bufnr)
 
 	-- Keep internal formatting for `gq`
 	-- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1131
@@ -206,12 +214,14 @@ null_ls.setup({
 		diagnostics.rubocop.with(rubocop_options),
 		diagnostics.shellcheck,
 		diagnostics.standardrb.with(standardrb_options),
-		diagnostics.vale.with({ filetypes = {
-			"gitcommit",
-			"markdown",
-			"tex",
-			"asciidoc",
-		} }),
+		diagnostics.vale.with({
+			filetypes = {
+				"gitcommit",
+				"markdown",
+				"tex",
+				"asciidoc",
+			},
+		}),
 		diagnostics.zsh,
 		formatting.eslint.with(eslint_options),
 		formatting.prettierd.with(prettierd_opts),
