2c3529d4f59bc46d214a7b8ba07880ed0413e3ab
Jesse Bailey <jesse@jsilasbailey.com>
2023-04-21 13:40:42 -0400

    (nvim) Remove unused configuration from moving to lazy.nvim

--- config/nvim/lua/jb/plugins/config/splitjoin.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-function M.config()
-  vim.g.splitjoin_ruby_curly_braces = 0
-  vim.g.splitjoin_ruby_hanging_args = 0
-  vim.g.splitjoin_ruby_options_as_arguments = 1
-end
-
-return M
