298e7fd9bc97e5fbbedeaa6ff0e826ccc479b0b1
Jesse Bailey <jesse@jsilasbailey.com>
2023-04-21 12:56:05 -0400

    (wezterm) Add support for split_nav
    Send pane navigation to `nvim` and `vim` or `tmux` when appropriate so
    that the same navigation keys for panes can be re-used between wezterm
    and those programs running within wezterm.

--- wezterm.lua
+++ wezterm.lua
@@ -19,5 +19,66 @@ config.inactive_pane_hsb = {
 	brightness = 0.5,
 }
 
+-- Equivalent to POSIX basename(3)
+-- Given "/foo/bar" returns "bar"
+-- Given "c:\\foo\\bar" returns "bar"
+local function basename(s)
+	return string.gsub(s, "(.*[/\\])(.*)", "%2")
+end
+
+local function pass_cmd_to_pane(pane)
+	local process_name = basename(pane:get_foreground_process_name())
+	return process_name == "nvim" or process_name == "vim" or process_name == "tmux"
+end
+
+local direction_keys = {
+	Left = "h",
+	Down = "j",
+	Up = "k",
+	Right = "l",
+	-- reverse lookup
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if pass_cmd_to_pane(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = {
+						key = key,
+						mods = resize_or_move == "resize" and "META" or "CTRL",
+					},
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
+config.keys = {
+	-- move between split panes
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
+	-- resize panes
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
+}
+
 -- and finally, return the configuration to wezterm
 return config
