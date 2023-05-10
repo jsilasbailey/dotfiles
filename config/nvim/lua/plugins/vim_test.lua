8701ad409a563daf8725d0356386d442af0572e2
Jesse Bailey <jesse@jsilasbailey.com>
2023-05-10 16:11:27 -0400

    (nvim) vim-test update default strategy
    - Update the default strategy for vim-test to "neovim" which will open
      the test focus in a `:term` window. Usage of "vim-dispatch" was very
      nice in a tmux window but broke down if not in a tmux window as it
      would run an external command and I wouldn't have access to the
      console during bindings. Since I'm always using "neovim", using its
      builtin terminal is OK mostly and works whether in tmux or not.
    - Update test keybinding descriptions. Which-key will display these when
      showing possible next characters for key chords.

--- config/nvim/lua/plugins/vim_test.lua
+++ config/nvim/lua/plugins/vim_test.lua
@@ -2,13 +2,29 @@ return {
   "https://github.com/vim-test/vim-test",
   requires = "https://github.com/tpope/vim-dispatch",
   config = function()
-    vim.g["test#strategy"] = "dispatch"
+    -- Async test execution
+    --
+    -- CustomStrategy to return focus to current window
+    --
+    -- https://github.com/vim-test/vim-test/issues/448
+    -- function! CustomStrategy(cmd)
+    --   execute 'bel 10 new'
+    --   call termopen(a:cmd)
+    --   wincmd p " switch back to last window
+    -- endfunction
+    --
+    -- let test#custom_strategies = {'custom': function('CustomStrategy')}
+    -- let test#strategy = "custom"
+    --
+    -- OR use neotest for lua async strategies
+    -- https://github.com/nvim-neotest/neotest
+    vim.g["test#strategy"] = "neovim"
   end,
   keys = {
-    { "<leader>tn", ":TestNearest<cr>", desc = "Dispatch test nearest to line" },
-    { "<leader>tf", ":TestFile<cr>", desc = "Dispatch test file" },
-    { "<leader>ta", ":TestSuite<cr>", desc = "Dispatch test all tests" },
-    { "<leader>tl", ":TestSuite<cr>", desc = "Dispatch test last test" },
+    { "<leader>tn", ":TestNearest<cr>", desc = "Test nearest to line" },
+    { "<leader>tf", ":TestFile<cr>", desc = "Test file" },
+    { "<leader>ta", ":TestSuite<cr>", desc = "Test all tests" },
+    { "<leader>tl", ":TestLast<cr>", desc = "Test last test" },
     { "<leader>gt", ":TestVisit<cr>", desc = "Go to test" },
   },
 }
