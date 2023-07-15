e11b96f023bb414b9f5cb3d54fc2ac84b056e2bb
Jesse Bailey <jesse@jsilasbailey.com>
2023-07-15 16:07:41 -0400

    (nvim) Fix reference bug

--- config/nvim/lua/plugins/octo.lua
+++ config/nvim/lua/plugins/octo.lua
@@ -2,7 +2,7 @@ return {
   "pwntester/octo.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope",
+    "nvim-telescope/telescope.nvim",
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
