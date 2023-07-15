83013d3d11ae0983a44dfc5721387f66dc6f1f7a
Jesse Bailey <jesse@jsilasbailey.com>
2023-07-15 15:33:28 -0400

    (nvim) Move smartsplits config to module

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -22,26 +22,7 @@ return {
   "tpope/vim-rake",
   {
     "mrjones2014/smart-splits.nvim",
-    config = function()
-      -- recommended mappings
-      -- resizing splits
-      -- these keymaps will also accept a range,
-      -- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
-      vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
-      vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
-      vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
-      vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
-      -- moving between splits
-      vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
-      vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
-      vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
-      vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
-      -- swapping buffers between windows
-      vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
-      vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
-      vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
-      vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
-    end,
+    config = require("plugins.config.smartsplits").config,
   },
   "AndrewRadev/splitjoin.vim", -- Still need for erb files while not using treesitter on them
   "nvim-tree/nvim-web-devicons",
