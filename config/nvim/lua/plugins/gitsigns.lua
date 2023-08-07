60d78d2f50abb4eb757bc430fd2aae3eaf7f08af
Jesse Bailey <jesse@jsilasbailey.com>
2023-08-07 19:13:21 -0400

    (nvim) Change gitsigns to be simpler

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -5,8 +5,8 @@ return {
       signs = {
         add = { text = "│" },
         change = { text = "│" },
-        delete = { text = "-v" },
-        topdelete = { text = "-^" },
+        delete = { text = "-" },
+        topdelete = { text = "-" },
         changedelete = { text = "~" },
         untracked = { text = "┆" },
       },
