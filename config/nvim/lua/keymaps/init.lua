1f7ac4557217717a336afd9c4a4bf0a42da35a5e
Jesse Bailey <dev@jsilasbailey.com>
2024-02-19 10:04:03 -0500

    Update window resizing and line movement

--- config/nvim/lua/keymaps/init.lua
+++ config/nvim/lua/keymaps/init.lua
@@ -11,7 +11,7 @@ keymap("n", "<leader>q", ":q<cr>", { silent = true })
 -- Save and kill windows with leader
 keymap("n", "<leader>wq", ":wq<cr>", { silent = true })
 
--- Cursor reamins in center of the screen when page jumping
+-- Cursor remains in center of the screen when page jumping
 keymap("n", "<C-d>", "<C-d>zz", { silent = true })
 keymap("n", "<C-u>", "<C-u>zz", { silent = true })
 
@@ -22,10 +22,17 @@ keymap("v", ">", ">gv", { silent = true })
 keymap("v", "p", '"_dP', { silent = true })
 
 -- Move text up and down
+keymap("n", "<A-j>", ":m .+1<CR>==", { silent = true })
+keymap("n", "<A-k>", ":m .-2<CR>==", { silent = true })
 keymap("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
 keymap("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", { silent = true })
 keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", { silent = true })
+
+-- Windows
+keymap("n", "<A-l>", "10<C-w><", { silent = true })
+keymap("n", "<A-h>", "10<C-w>>", { silent = true })
+
 
 -- Terminal --
 -- Better terminal navigation
