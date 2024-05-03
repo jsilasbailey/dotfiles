8523c792302b3029eefa085e7b365b4595eaf36a
Jesse Bailey <dev@jsilasbailey.com>
2024-05-03 11:13:13 -0400

    Use C-y, C-l, C-h
    For completions:
    
    C-y: Confirm selection
    C-l: Snippet section next
    C-h: Snippet section previous

--- config/nvim/lua/plugins/config/nvim_cmp.lua
+++ config/nvim/lua/plugins/config/nvim_cmp.lua
@@ -5,7 +5,6 @@ M.setup = function()
   -- TODO: Change to luasnip? Learn how to build your own snippets.
   local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
   local cmp_git = require("cmp_git")
-
   cmp_git.setup()
 
   -- Provide text completion from all open buffers
@@ -25,31 +24,31 @@ M.setup = function()
         vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
       end,
     },
+    completion = { completeopt = "menu,menuone,noinsert" },
     mapping = cmp.mapping.preset.insert({
-      ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-      ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-      ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-      -- TODO: Use this
-      ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+      ["<C-Space>"] = cmp.mapping.complete({ select = true }),
       ["<C-e>"] = cmp.mapping({
         i = cmp.mapping.abort(),
         c = cmp.mapping.close(),
       }),
-      ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      ["<Tab>"] = cmp.mapping(function(fallback)
+      ["<C-l>"] = cmp.mapping(function(fallback)
         cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
       end, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
+      ["<C-h>"] = cmp.mapping(function(fallback)
         cmp_ultisnips_mappings.jump_backwards(fallback)
       end, { "i", "s" }),
     }),
-    sources = cmp.config.sources(
-      {
-        { name = "nvim_lsp" },
-        { name = "ultisnips" },
-      },
-      { { name = "buffer" } }
-    )
+    sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "ultisnips" },
+    }, {
+      { name = "buffer" },
+    }),
   })
 
   -- Set configuration for specific filetype.
