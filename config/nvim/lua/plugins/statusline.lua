91cfce1a726d09e327f77a13ac123da33b28ce0c
Jesse Bailey <dev@jsilasbailey.com>
2024-05-10 11:33:42 -0400

    Status line updates
    - Longer filenenames
    - Move branch to tabline top-right
    - Better tab info
    - Less information in statusline

--- config/nvim/lua/plugins/statusline.lua
+++ config/nvim/lua/plugins/statusline.lua
@@ -2,6 +2,31 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {}
-  }
+    opts = {
+      tabline = {
+        lualine_a = {},
+        lualine_b = { { "tabs", mode = 2, use_mode_colors = true } },
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { "branch" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "diff", "diagnostics" },
+        lualine_c = { { "filename", path = 4 } },
+        lualine_x = {},
+        lualine_y = { "filetype" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+    },
+  },
 }
