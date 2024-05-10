8d95a6ad3e6d70e7cc5fb0332962d3861aac41a3
Jesse Bailey <dev@jsilasbailey.com>
2024-05-10 11:32:30 -0400

    Add diagnostic keymaps

--- config/nvim/lua/keymaps/init.lua
+++ config/nvim/lua/keymaps/init.lua
@@ -1,41 +1,48 @@
-local keymap = vim.keymap.set
+local map = vim.keymap.set
 
 -- Remap for dealing with word wrap
-keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Save with leader
-keymap("n", "<leader>w", ":w<cr>", { silent = true })
+map("n", "<leader>w", ":w<cr>", { silent = true })
 -- Kill windows with leader
-keymap("n", "<leader>q", ":q<cr>", { silent = true })
+map("n", "<leader>q", ":q<cr>", { silent = true })
 -- Save and kill windows with leader
-keymap("n", "<leader>wq", ":wq<cr>", { silent = true })
+map("n", "<leader>wq", ":wq<cr>", { silent = true })
 
 -- Cursor remains in center of the screen when page jumping
-keymap("n", "<C-d>", "<C-d>zz", { silent = true })
-keymap("n", "<C-u>", "<C-u>zz", { silent = true })
+map("n", "<C-d>", "<C-d>zz", { silent = true })
+map("n", "<C-u>", "<C-u>zz", { silent = true })
 
 -- Stay in indent mode
-keymap("v", "<", "<gv", { silent = true })
-keymap("v", ">", ">gv", { silent = true })
+map("v", "<", "<gv", { silent = true })
+map("v", ">", ">gv", { silent = true })
 
-keymap("v", "p", '"_dP', { silent = true })
+map("v", "p", '"_dP', { silent = true })
 
 -- Move text up and down
-keymap("n", "<A-j>", ":m .+1<CR>==", { silent = true })
-keymap("n", "<A-k>", ":m .-2<CR>==", { silent = true })
-keymap("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
-keymap("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
-keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", { silent = true })
-keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", { silent = true })
+map("n", "<A-j>", ":m .+1<CR>==", { silent = true })
+map("n", "<A-k>", ":m .-2<CR>==", { silent = true })
+map("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+map("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { silent = true })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { silent = true })
 
 -- Windows
-keymap("n", "<A-l>", "10<C-w><", { silent = true })
-keymap("n", "<A-h>", "10<C-w>>", { silent = true })
+map("n", "<A-l>", "10<C-w><", { silent = true })
+map("n", "<A-h>", "10<C-w>>", { silent = true })
 
 -- Terminal --
 -- Better terminal navigation
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", { silent = true })
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", { silent = true })
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", { silent = true })
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", { silent = true })
+map("t", "<C-h>", "<C-\\><C-N><C-w>h", { silent = true })
+map("t", "<C-j>", "<C-\\><C-N><C-w>j", { silent = true })
+map("t", "<C-k>", "<C-\\><C-N><C-w>k", { silent = true })
+map("t", "<C-l>", "<C-\\><C-N><C-w>l", { silent = true })
+map("t", "<C-o>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Diagnostic keymaps
+map("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+map("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+map("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show [D]iagnostic messages" })
+map("n", "<leader>dq", vim.diagnostic.setloclist, { desc = "Open [D]iagnostic [Q]uickfix list" })
