58dbba8b9f3e24007b0af48a6ce64c61818e2fee
Jesse Bailey <dev@jsilasbailey.com>
2024-06-28 12:55:32 -0400

    Upgrade plugins
    - Update conform
    
    lsp_fallback option changed keys to allow for more lsp_format option
    types
    
    - Update gitsigns
    - Upgrade lazy-nvim
    - Upgrade leap
    - Upgrade nvim-lspconfig
    
    - Update neodev
    
    Will upgrade to lazydev.nvim when I upgrade nvim to 0.10

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,27 +1,35 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
+    lazyrepo,
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup(
-  "plugins",
-  {
-    change_detection = { notify = false },
-  }
-)
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- https://lazy.folke.io/
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "rose-pine" } },
+  -- Automatically check for plugin updates
+  checker = { enabled = true },
+})
 
 require("options")
 require("keymaps")
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -78,7 +78,7 @@ return {
         function()
           require("conform").format({
             async = true,
-            lsp_fallback = true,
+            lsp_format = "fallback",
           })
         end,
         mode = "",
@@ -132,7 +132,7 @@ return {
   {
     "ggandor/leap.nvim",
     config = function()
-      require("leap").add_default_mappings()
+      require("leap").create_default_mappings()
     end,
   },
   {
