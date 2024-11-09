b40754f46eece8c1ec61ccea592bd6353ca53afe
Jesse Bailey <dev@jsilasbailey.com>
2024-11-09 08:35:38 -0500

    Modify colorcolumn and cursorline
    Update to match rose-pine better and also be more subtle

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,3 +1,8 @@
+vim.cmd([[
+  highlight CursorLine gui=none cterm=none guibg=#201d2a
+  highlight ColorColumn guibg=#201d2a
+]])
+
 return {
   {
     "rose-pine/neovim",
@@ -5,6 +10,6 @@ return {
     branch = "main",
     config = function()
       vim.cmd("colorscheme rose-pine")
-    end
+    end,
   },
 }
