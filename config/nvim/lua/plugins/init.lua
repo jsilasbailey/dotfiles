52c01ed3637cc6bf3caa7dc640448a10ae6efb5e
Jesse Bailey <dev@jsilasbailey.com>
2024-11-09 10:23:44 -0500

    Use prettierd to format for speeds
    Running into an issue currently where prettierd will not sort
    tailwindcss classes the same as running prettier in the repo but hard to
    reproduce.

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -105,13 +105,13 @@ return {
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
-        javascript = { { "prettierd", "prettier" } },
-        typescript = { { "prettierd", "prettier" } },
-        typescriptreact = { { "prettierd", "prettier" } },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
       },
       format_on_save = {
         timeout_ms = 500,
-        lsp_fallback = true,
+        lsp_format = "fallback",
       },
     },
   },
