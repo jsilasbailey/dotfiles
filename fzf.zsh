7bd631f2fa42d762e6ac82ef6ff223978886cd41
Jesse Bailey <dev@jsilasbailey.com>
2024-12-04 20:52:45 -0500

    Use FZF_DEFAULT_OPTS
    Control the layout whether in tmux or not using the FZF_DEFAULT_OPTS env
    variable.

--- fzf.zsh
+++ fzf.zsh
@@ -4,6 +4,9 @@ if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
+# Open in tmux popup if on tmux, otherwise use --height mode
+export FZF_DEFAULT_OPTS='--height 20% --tmux bottom,40% --layout reverse --border top'
+
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
