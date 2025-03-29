dd6eae3595132977c93f28fa551d70034999f26b
Jesse Bailey <dev@jsilasbailey.com>
2025-03-29 10:15:25 -0400

    Center floating fzf window by default

--- fzf.zsh
+++ fzf.zsh
@@ -5,7 +5,7 @@ if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
 fi
 
 # Open in tmux popup if on tmux, otherwise use --height mode
-export FZF_DEFAULT_OPTS='--height 20% --tmux bottom,40% --layout reverse --border top'
+export FZF_DEFAULT_OPTS='--height 20% --layout reverse --border --tmux 50% --inline-info'
 
 # Auto-completion
 # ---------------
