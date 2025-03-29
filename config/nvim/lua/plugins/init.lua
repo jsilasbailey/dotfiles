e8c344d5198af427ce86c9fbd2498de87233ba63
Jesse Bailey <dev@jsilasbailey.com>
2025-03-29 09:54:02 -0400

    Add wezterm lua types for configuration

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -255,9 +255,13 @@ return {
   {
     "folke/lazydev.nvim",
     ft = "lua",
+    dependencies = {
+      { "gonstoll/wezterm-types", lazy = true },
+    },
     opts = {
       library = {
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        { path = "wezterm-types", mods = { "wezterm" } },
       },
     },
   },
