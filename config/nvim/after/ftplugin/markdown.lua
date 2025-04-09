0f027764701d8b19eb4caa2f8356ddedee8ea2fd
Jesse Bailey <dev@jsilasbailey.com>
2025-04-09 11:05:51 -0400

    Only apply markdown editing options for modifiable

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -1,24 +1,26 @@
-vim.opt_local.spell = true
-vim.opt_local.textwidth = 100
+if vim.o.modifiable then
+  vim.api.nvim_set_option_value("spell", true, { scope = "local" })
+  vim.api.nvim_set_option_value("textwidth", 100, { scope = "local" })
 
--- Function to toggle markdown checkboxes
-local function toggle_checkbox()
-  -- Get current line
-  local line = vim.api.nvim_get_current_line()
+  -- Function to toggle markdown checkboxes
+  local function toggle_checkbox()
+    -- Get current line
+    local line = vim.api.nvim_get_current_line()
 
-  -- Check if the line contains an unchecked checkbox
-  if line:match("- %[ %]") then
-    -- Replace unchecked with checked
-    local new_line = line:gsub("- %[ %]", "- [x]", 1)
-    vim.api.nvim_set_current_line(new_line)
-  -- Check if the line contains a checked checkbox
-  elseif line:match("- %[x%]") then
-    -- Replace checked with unchecked
-    local new_line = line:gsub("- %[x%]", "- [ ]", 1)
-    vim.api.nvim_set_current_line(new_line)
+    -- Check if the line contains an unchecked checkbox
+    if line:match("- %[ %]") then
+      -- Replace unchecked with checked
+      local new_line = line:gsub("- %[ %]", "- [x]", 1)
+      vim.api.nvim_set_current_line(new_line)
+      -- Check if the line contains a checked checkbox
+    elseif line:match("- %[x%]") then
+      -- Replace checked with unchecked
+      local new_line = line:gsub("- %[x%]", "- [ ]", 1)
+      vim.api.nvim_set_current_line(new_line)
+    end
   end
-end
 
--- Map <leader>x to toggle checkbox function
--- vim.api.nvim_set_keymap("n", "<leader>x", toggle_checkbox, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>x", toggle_checkbox, { noremap = true, silent = true })
+  -- Map <leader>x to toggle checkbox function
+  -- vim.api.nvim_set_keymap("n", "<leader>x", toggle_checkbox, { noremap = true, silent = true })
+  vim.keymap.set("n", "<leader>x", toggle_checkbox, { noremap = true, silent = true })
+end
