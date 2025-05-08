1fd9c889f266537e4eb1740c3ad46f08eb375e8c
Jesse Bailey <dev@jsilasbailey.com>
2025-05-08 16:41:17 -0400

    Use default nvim keymaps for lsp fnctions

--- config/nvim/lua/plugins/config/lspconfig.lua
+++ config/nvim/lua/plugins/config/lspconfig.lua
@@ -87,20 +87,10 @@ M.setup = function()
     group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
 
     callback = function(event)
-      local map = function(keys, func, desc)
-        vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-      end
-
-      map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-      map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-      map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-      map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-      map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-      map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-      map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-      map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-      map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+      -- TODO: If supports go to def
+      vim.keymap.set("n", "gd", function()
+        vim.lsp.buf.definition()
+      end, { buffer = event.buf, desc = "LSP: [G]o to [D]efinition" })
     end,
   })
 end
