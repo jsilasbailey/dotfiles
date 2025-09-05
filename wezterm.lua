6e793e326f7e76a2d4a87670a7fcd4ff680bccf4
Jesse Bailey <dev@jsilasbailey.com>
2025-09-05 10:14:26 -0400

    (wezterm) Remove bell config

--- wezterm.lua
+++ wezterm.lua
@@ -10,28 +10,17 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+config.font_size = 12
+
 config.color_scheme_dirs = {
   wezterm.home_dir .. "/.config/wezterm/color_schemes",
 }
 config.color_scheme = "Zenbones_dark"
 
-config.font_size = 12
-
 config.inactive_pane_hsb = {
+  hue = 1.0,
   saturation = 0.7,
   brightness = 0.5,
-}
-
-config.colors = {
-  visual_bell = "#202020",
-}
-
-config.visual_bell = {
-  fade_in_function = "EaseIn",
-  fade_in_duration_ms = 150,
-  fade_out_function = "EaseOut",
-  fade_out_duration_ms = 150,
-  target = "CursorColor",
 }
 
 config.audible_bell = "Disabled"
@@ -40,6 +29,7 @@ config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
 config.show_new_tab_button_in_tab_bar = false
+
 config.window_decorations = "RESIZE"
 
 -- and finally, return the configuration to wezterm
