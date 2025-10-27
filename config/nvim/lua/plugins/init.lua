ef2bdb83709e5011f15a64ca1549f62c10de4204
Jesse Bailey <dev@jsilasbailey.com>
2025-10-27 11:40:09 -0400

    (nvim-metals) Formatting

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -331,6 +331,7 @@ return {
           metals.setup_dap()
 
           local metals_config = metals.bare_config()
+
           -- "off" will enable LSP progress notifications by Metals and you'll need
           -- to ensure you have a plugin like fidget.nvim installed to handle them.
           --
@@ -338,6 +339,7 @@ return {
           -- a have settings to capture this in your statusline or else you'll not see
           -- any messages from metals. There is more info in the help docs about this
           metals_config.init_options.statusBarProvider = "off"
+
           metals_config.settings = {
             showImplicitArguments = true,
             showImplicitConversionsAndClasses = true,
@@ -357,6 +359,7 @@ return {
             vim.lsp.protocol.make_client_capabilities(),
             require("cmp_nvim_lsp").default_capabilities()
           )
+
           metals_config.on_attach = function(_client, bufnr)
             vim.keymap.set("n", "<leader>cl", vim.lsp.codelens.run, {
               buffer = bufnr,
