5ab16132fdfaf88e62e6e61768bcae5d893d59c4
Jesse Bailey <dev@jsilasbailey.com>
2026-02-27 09:20:43 -0500

    (theme) Use Koda
    A delightful light palette for day coding use! Minimal, slight highlighting,
    feels a bit like paper. The dark theme isn't bad either!

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,70 @@
+local read_apple_interface_style =
+  "defaults read -g AppleInterfaceStyle 2>/dev/null"
+
+-- Function to set background based on macOS system theme
+local function set_background_from_system()
+  local handle = io.popen(read_apple_interface_style)
+
+  if handle then
+    local result = handle:read("*a")
+    handle:close()
+
+    if result:match("Dark") then
+      vim.opt.background = "dark"
+    else
+      vim.opt.background = "light"
+    end
+  else
+    vim.opt.background = "light"
+  end
+end
+
+local function setup_auto_background_change()
+  -- Set background on startup
+  set_background_from_system()
+
+  -- Update background when Neovim gains focus
+  vim.api.nvim_create_autocmd({ "FocusGained", "VimEnter" }, {
+    callback = function()
+      set_background_from_system()
+    end,
+  })
+end
+
 return {
+  {
+    "oskarnurm/koda.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      setup_auto_background_change()
+      require("koda").setup()
+      vim.cmd("colorscheme koda")
+    end,
+  },
   {
     "zenbones-theme/zenbones.nvim",
     dependencies = "rktjmp/lush.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.opt.background = "dark"
-      vim.g.zenbones = { darkness = "stark" }
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --   Set background on startup
+    --   set_background_from_system()
 
-      vim.cmd([[colorscheme zenbones]])
+    --   Update background when Neovim gains focus
+    --   vim.api.nvim_create_autocmd({ "FocusGained", "VimEnter" }, {
+    --     callback = function()
+    --       set_background_from_system()
+    --     end,
+    --   })
 
-      local normal_float = vim.api.nvim_get_hl(0, { name = "NormalFloat" })
-      vim.api.nvim_set_hl(0, "FloatBorder", {
-        fg = normal_float.fg,
-        bg = normal_float.bg,
-      })
-    end,
+    --   vim.cmd([[colorscheme zenwritten]])
+
+    --   local normal_float = vim.api.nvim_get_hl(0, { name = "NormalFloat" })
+    --   vim.api.nvim_set_hl(0, "FloatBorder", {
+    --     fg = normal_float.fg,
+    --     bg = normal_float.bg,
+    --   })
+    -- end,
   },
 }
